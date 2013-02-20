c07ff15c33d3bfb44603dec6c8ef16282f82093d
TobiasParent <parent.tobias@gmail.com>
2013-02-20 14:18:49 -0500

    Added namespaces

--- assets/javascripts/darts.tourneys.js
+++ assets/javascripts/darts.tourneys.js
@@ -1,3 +1,9 @@
 jQuery(function() {
-
+  // Create our namespace and related organizers
+  window.DartsLeague = {};
+  DartsLeague.Models = {};
+  DartsLeague.Collections = {};
+  DartsLeague.Views = {};
+  
+  
 });
