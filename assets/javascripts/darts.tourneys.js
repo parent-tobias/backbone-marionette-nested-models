6fdb624860bc2e7f21a535886bcba7fd326a6243
TobiasParent <parent.tobias@gmail.com>
2013-02-20 14:26:05 -0500

    Added Member Model and Collection

--- assets/javascripts/darts.tourneys.js
+++ assets/javascripts/darts.tourneys.js
@@ -5,5 +5,9 @@ jQuery(function() {
   DartsLeague.Collections = {};
   DartsLeague.Views = {};
   
-  
+  DartsLeague.Models.Member = Backbone.Model.extend({ 
+  });
+  DartsLeague.Collections.Members = Backbone.Collection.extend({
+      model: DartsLeague.Models.Member
+  });
 });
